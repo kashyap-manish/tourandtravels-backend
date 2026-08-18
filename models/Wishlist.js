ee996492ab4f51065e551ac36d64747eb87b7533
Manish Kumar <kumarthakurmanish1999@gmail.com>
2026-08-18 17:54:16 +0530

    update routes and models

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -3,6 +3,15 @@ const { Schema, model } = require('mongoose');
 const wishlistSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
   tours: [{ type: Schema.Types.ObjectId, ref: 'Tour' }],
+  hotels: [{
+    id:       { type: String, required: true },
+    title:    String,
+    img:      String,
+    price:    String,
+    location: String,
+    stars:    Number,
+    website:  String,
+  }],
 }, { timestamps: true });
 
 module.exports = model('Wishlist', wishlistSchema);
--- routes/wishlist.js
+++ routes/wishlist.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/auth');
 router.get('/', auth, async (req, res) => {
   try {
     const wishlist = await Wishlist.findOne({ user: req.user.id }).populate('tours', 'title img price days location');
-    res.json(wishlist || { tours: [] });
+    res.json(wishlist || { tours: [], hotels: [] });
   } catch (e) {
     res.status(500).json({ message: e.message });
   }
@@ -16,10 +16,26 @@ router.get('/', auth, async (req, res) => {
 router.post('/toggle/:tourId', auth, async (req, res) => {
   try {
     let wishlist = await Wishlist.findOne({ user: req.user.id });
-    if (!wishlist) wishlist = await Wishlist.create({ user: req.user.id, tours: [] });
+    if (!wishlist) wishlist = await Wishlist.create({ user: req.user.id, tours: [], hotels: [] });
     const idx = wishlist.tours.indexOf(req.params.tourId);
     if (idx > -1) wishlist.tours.splice(idx, 1);
     else wishlist.tours.push(req.params.tourId);
+    await wishlist.save();
+    res.json(wishlist);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+// Toggle hotel in wishlist
+router.post('/toggle-hotel', auth, async (req, res) => {
+  try {
+    const { id, title, img, price, location, stars, website } = req.body;
+    let wishlist = await Wishlist.findOne({ user: req.user.id });
+    if (!wishlist) wishlist = await Wishlist.create({ user: req.user.id, tours: [], hotels: [] });
+    const idx = wishlist.hotels.findIndex(h => h.id === id);
+    if (idx > -1) wishlist.hotels.splice(idx, 1);
+    else wishlist.hotels.push({ id, title, img, price, location, stars, website });
     await wishlist.save();
     res.json(wishlist);
   } catch (e) {
